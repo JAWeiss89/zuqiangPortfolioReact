7529f019867d80846c2c559318637111fe26d1c3
Jorge Weiss <jorge.a.weiss@gmail.com>
2020-12-08 21:49:11 -0600

    create film image bar

--- src/Film.js
+++ src/Film.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 
 const Film = ( {films} ) => {
     const {filmID} = useParams();
     const film = films.find(item => item.id === filmID)
+    const [imageFeatured, setImageFeatured] = useState(film.mainimage);
+
+    const handleImageClick = (event) => {
+        setImageFeatured(event.target.attributes.source.value);
+    }
+    
+    useEffect(() => {
+        setImageFeatured(film.mainimage);
+    }, [filmID, film.mainimage])
+
     return (
         <section id="content">
             <h1 className="title">{film.title}</h1>
-            <img className="main-img" src={film.mainimage} alt={film.title}/>
+            <img className="main-img" src={imageFeatured} alt={film.title}/>
+            {film.images &&
+            <div className="image-bar">
+                {film.images.map((image, i) => {
+                    return (
+                        <img src={image} source={image} onClick={handleImageClick} key={`${film.title}-${i}`} alt={`${film.title}-${i}`}  />
+                    )
+                })}
+            </div>
+            }
             <p id="specs">{film.specs}</p>
             <p id="details">{film.details}</p>
             {film.summaryparagraphs.map((paragraph, i)=> <p key={i}>{paragraph}</p>)}
-
+            
             {film.videosrc &&
             <iframe src={film.videosrc} title={film.title} width="640" height="360" frameBorder="0" allow="autoplay; fullscreen" ></iframe>            
             }
