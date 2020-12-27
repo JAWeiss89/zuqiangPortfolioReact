30d40c79342feb78e0d07c438a856dbc651186ac
Jorge Weiss <jorge.a.weiss@gmail.com>
2020-12-26 23:44:46 -0600

    Add details for Keep In Touch and update info and landing

--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,14 @@ const Home = ({hideNav}) => {
     return (
         <section onClick={hideNav} id="content">
             <h1 id="title" className="hidden">Peng Zuqiang</h1>
-            <img className="main-img" src="./images/landing.jpg" alt="landing"/>
+            {/* <img className="main-img" src="./images/landing.jpg" alt="landing"/> */}
             <p><b>News :</b></p>
+            <p>Awarded 'Jury Speical Prize' at Huayu Youth Award 2020</p>
+
+            <p><Link to="/nan"><em>Nan</em></Link> received a 'Special Mention' in International Feature-length Documentary Competition at Festival Film Dokumenter, 2020</p>
+
+            <p>“The Final Prophet” - Shenzhen Bat STArts Festival, China, Dec.26, 2020 - Jan. 26, 2021</p>
+            
             <p>World Premier of <Link to="/nan"><em>Nan</em></Link> at <a href="https://www.idfa.nl/en/film/148171ce-6a64-4764-ad57-76b16f626d64/nan">First Appearance section, IDFA</a> , Nov.18 - Dec.6, 2020</p>
 
             <p>Asia Premier of <Link to="/nan"><em>Nan</em></Link> at <a href="https://ffd.or.id/film/nan/?lang=en">Festival Film Dokumenter</a>, International Feature-length Documentary Competition, Nov.25 - Dec.14, 2020</p>
--- src/Info.js
+++ src/Info.js
@@ -4,11 +4,12 @@ const Info = ({hideNav}) => {
     return  (   
         <section onClick={hideNav} id="content">
             <h1 id="title" className="hidden">info</h1>
-            <img className="main-img" src="./images/info.jpg" alt="info"/>
+            {/* <img className="main-img" src="./images/info.jpg" alt="info"/> */}
             <p>zuqiangpeng@gmail.com | <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1lmyM9UjpoS4ztlFPKtjNQ9ciHQHyG9pX--Q7KzkBAc0/edit?usp=sharing"><span>CV</span></a></p>
             <p>-</p>
             <p>Peng Zuqiang makes moving images.</p>
-            <p>Zuqiang‘s works have been shown at exhibitions and festivals internationally including Antimatter, Dok Leipzig, Open City Documentary Festival, Connecting Space in Hong Kong, and Museum of Fine Arts, Houston. He has received fellowships and residencies from the MacDowell, Skowhegan, True/False Film Festival, and the Core Program. Zuqiang is part of the film collective Fish in a Pot 🐠.</p>
+            {/* <p>Zuqiang‘s works have been shown at exhibitions and festivals internationally including Antimatter, Dok Leipzig, Open City Documentary Festival, Connecting Space in Hong Kong, and Museum of Fine Arts, Houston. He has received fellowships and residencies from the MacDowell, Skowhegan, True/False Film Festival, and the Core Program. Zuqiang is part of the film collective Fish in a Pot 🐠.</p> */}
+            <p>Zuqiang’s works have been shown at exhibitions and festivals internationally including Antimatter, Open City Documentary Festival, IDFA, UCCA Beijing, and Museum of Fine Arts, Houston. He has received fellowships and residencies from the MacDowell, Skowhegan, and the Core Program and the Lighthouse Works. He received the ‘Jury Special Prize’ from the 8th Huayu Youth Award, and a ‘Special Mention’ from Festival Film Dokumenter, Yogyakarta for his first feature film.</p>
         </section>
     )
 }
