fce0a7195ab01b5709ed160b0c8a1e6495cf5c25
Jorge Weiss <jorge.a.weiss@gmail.com>
2021-03-31 00:18:38 -0500

    Update news and remove video from Surface Value

--- src/Home.js
+++ src/Home.js
@@ -8,11 +8,13 @@ const Home = ({hideNav}) => {
             <h1 id="title" className="hidden">Peng Zuqiang</h1>
             {/* <img className="main-img" src="./images/landing.jpg" alt="landing"/> */}
             <p><b>News :</b></p>
-            <p>Awarded 'Jury Speical Prize' at Huayu Youth Award 2020</p>
+            <p>Right of Return, Deluge Contemporary Art, Victoria, Canada. April 10 to May 8, 2021</p>
 
+            <p>Awarded 'Jury Special Prize' at Huayu Youth Award 2020</p>
+            
             <p><Link to="/nan"><em>Nan</em></Link> received a 'Special Mention' in International Feature-length Documentary Competition at Festival Film Dokumenter, 2020</p>
 
-            <p>“The Final Prophet” - Shenzhen Bat STArts Festival, China, Dec.26, 2020 - Jan. 26, 2021</p>
+            
 
         </section>
     )
