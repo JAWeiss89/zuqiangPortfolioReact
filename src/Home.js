eb4a7cfe1ee6a56416fbbdb175777290d9c6d98c
Jorge Weiss <jorge.a.weiss@gmail.com>
2021-07-26 21:31:58 -0500

    Change landing page image width to 60% on mobile

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+//import {Link} from 'react-router-dom';
 // import landing from '/images/landing.jpg';
 
 const Home = ({hideNav}) => {
@@ -8,11 +8,11 @@ const Home = ({hideNav}) => {
             <h1 id="title" className="hidden">Peng Zuqiang</h1>
             
             <p><b>News :</b></p>
-            <p>Peng Zuqiang : <em>Hesitations,</em> ANTENNA-TENNA, Shanghai. July 31 - September 5, 2021</p>
+            <p>Peng Zuqiang : <em>Hesitations,</em> ANTENNA-TENNA, Shanghai. <br/>July 31 - September 5, 2021</p>
             <img className="main-img" src="./images/poster.jpg" alt="exhibition poster"/>
-            <p><em>Más allá el mar canta,</em> Times Art Centre in Berlin, Germany. Curated by Pablo José Ramirez. September 15 - December 18, 2021.</p>
+            <p><em>Más allá el mar canta,</em> Times Art Centre in Berlin, Germany. <br/>Curated by Pablo José Ramirez. <br/>September 15 - December 18, 2021.</p>
             
-            <p><Link to="/nan"><em>Nan</em></Link> received a 'Special Mention' in International Feature-length Documentary Competition at Festival Film Dokumenter, 2020</p>
+            {/* <p><Link to="/nan"><em>Nan</em></Link> received a 'Special Mention' in International Feature-length Documentary Competition at Festival Film Dokumenter, 2020</p> */}
 
             
 
