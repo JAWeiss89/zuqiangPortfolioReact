607ba2091a32b4de388c3fd94b881c03fd9f1f13
Jorge Weiss <jorge.a.weiss@gmail.com>
2022-03-30 00:57:17 -0500

    Add sideways looking and update sight film

--- src/Home.js
+++ src/Home.js
@@ -13,6 +13,13 @@ const Home = ({hideNav}) => {
             <p style={{margin:'0px', fontSize:'12px'}}>Guangdong Times Museum, Guangzhou </p>
             <p style={{margin:'0px', fontSize:'12px'}}>December 4, 2021 - January 30, 2022</p>
             <img className="landing-img" src="./images/one_song.png" alt="Exhibit Flyer"/> */}
+
+            <p style={{margin:'0px', fontSize:'14px'}}><a href="https://www.cellprojects.org/exhibitions/peng-zuqiang" target="_blank" rel="noreferrer"><em>Sideways Looking</em></a></p>
+            <p style={{margin:'0px', fontSize:'12px'}}>Cell Project Space, London, UK </p>
+            <p style={{margin:'0px', fontSize:'12px'}}>April 13, 2022 - June 12, 2022</p>
+            <img className="landing-img" src="./images/sideways_looking.jpg" alt="exhibition poster"/>
+            <p></p>
+
             
             <p style={{margin:'0px', fontSize:'14px'}}><a href="http://www.macallineart.org/en/exhibitions/1076/the-elephant-escaped" target="_blank" rel="noreferrer"><em>The Elephant Escaped</em></a></p>
             <p style={{margin:'0px', fontSize:'12px'}}>Macalline Art Center, Beijing </p>
