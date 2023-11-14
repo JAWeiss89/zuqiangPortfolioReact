e29cd3532584e787d60ffa01500317a5ca6cd7de
geoblocked <72842644+geoblocked@users.noreply.github.com>
2023-11-14 12:57:37 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -37,7 +37,7 @@ const Home = ({hideNav}) => {
             <p style={{margin:'0px', fontSize:'12px'}}>October 28 - December 16, 2023</p>
             <p></p>
 
-            <p style={{margin:'0px', fontSize:'14px'}}><a href="https://www.kasselerdokfest.de/en/news/programmkatalog-ab-heute-online-1" target="_blank" rel="noreferrer"><em>Lost Archives II</em></a></p>
+            <p style={{margin:'0px', fontSize:'14px'}}><a href="https://www.kasselerdokfest.de/en/compilation/p-e6156d46-fb1b-8f49-bcf4-c5e216240d7f" target="_blank" rel="noreferrer"><em>Lost Archives II</em></a></p>
             <p style={{margin:'0px', fontSize:'12px'}}>40th Kasseler Dokfest</p>
             <p style={{margin:'0px', fontSize:'12px'}}>November 17, 2023</p>
             <p></p>
