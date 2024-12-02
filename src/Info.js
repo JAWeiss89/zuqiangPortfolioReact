1b6268f1aa70ff6dd36485fb656491c2a1bf912e
geoblocked <72842644+geoblocked@users.noreply.github.com>
2024-12-02 19:16:35 +0100

    Update Info.js

--- src/Info.js
+++ src/Info.js
@@ -13,10 +13,10 @@ const Info = ({hideNav}) => {
             {/* <p>Zuqiang‘s works have been shown at exhibitions and festivals internationally including Antimatter, Dok Leipzig, Open City Documentary Festival, Connecting Space in Hong Kong, and Museum of Fine Arts, Houston. He has received fellowships and residencies from the MacDowell, Skowhegan, True/False Film Festival, and the Core Program. Zuqiang is part of the film collective Fish in a Pot 🐠.</p> */}
             {/* <p>Zuqiang’s works have been shown at exhibitions and festivals internationally including Antimatter, Open City Documentary Festival, IDFA, UCCA Beijing, and Museum of Fine Arts, Houston. He has received fellowships and residencies from the MacDowell, Skowhegan, the Core Program and the Lighthouse Works. He received the ‘Jury Special Prize’ from the 8th Huayu Youth Award, and a ‘Special Mention’ from Festival Film Dokumenter, Yogyakarta for his first feature film.</p> */}
             {/* <p>Zuqiang’s works have been shown at exhibitions and festivals internationally including UCCA Beijing, Time Art Center Berlin, IDFA,  Open City Documentary Festival, and Antimatter. He has received fellowships and residencies from the MacDowell, Skowhegan, the Core Program and the Lighthouse Works. He received the ‘Jury Special Prize’ from the 8th Huayu Youth Award, and a ‘Special Mention’ from Festival Film Dokumenter, Yogyakarta for his first feature film, Nan (2020)</p> */}
-            <p> Recent solo presentations include Fondazione Sandretto (2023), Kevin Space (2023), Cell Project Space (2022), E-Flux screening room (2022). 
-                Group exhibitions and screenings include videobrasil, UCCA Beijing, Schirn Kunsthalle Frankfurt, Times Museum, Eye Film Museum, IDFA, 25FPS, Alchemy Film and Moving Image Festival. 
-                He is a recipient of the Present Future prize at Artissima (2022), and the Dialog Award at EMAF (2023, 2024). He completed residencies and fellowship from Rijksakademie, Skowhegan, the Core Program, and Art Explora. 
-                He lives and works in between Amsterdam and Paris.</p>
+            <p> Recent solo presentations include Fondazione Sandretto (2023), Kevin Space (2023), Cell Project Space (2022), E-Flux screening room (2022). </p>
+            <p> Group exhibitions and screenings include videobrasil, UCCA Beijing, Schirn Kunsthalle Frankfurt, Times Museum, Eye Film Museum, IDFA, 25FPS, Alchemy Film and Moving Image Festival. </p>
+            <p> He is a recipient of the Present Future prize at Artissima (2022), and the Dialog Award at EMAF (2023, 2024). He completed residencies and fellowship from Rijksakademie, Skowhegan, the Core Program, and Art Explora. </p>
+            <p> He lives and works in between Amsterdam and Paris.</p>
         </section>
     )
 }
