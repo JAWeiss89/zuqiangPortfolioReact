7d5f8bb6dd3f1b6528221070c0bee1e9b09f357f
geoblocked <72842644+geoblocked@users.noreply.github.com>
2024-12-10 15:19:56 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -45,16 +45,18 @@ const Home = ({hideNav}) => {
             <p style={{margin:'0px', fontSize:'12px'}}>Dec 9 - 22, 2024 </p>
             <p></p>
 
+            <p style={{margin:'0px', fontSize:'12px'}}>Screening </p>
+            <p style={{margin:'0px', fontSize:'12px'}}>Custom Guitars, Berlin </p>
+            <p style={{margin:'0px', fontSize:'12px'}}>Dec 14, 2024</p>
+            <p></p>
+
             <p style={{margin:'0px', fontSize:'14px'}}><a href="https://www.schermodellarte.org/en/visio-2024/" target="_blank" rel="noreferrer"><em> VISIO 13th EDITION
 European Programme on Artists’ Moving Images </em></a></p>
             <p style={{margin:'0px', fontSize:'12px'}}>Lo schermo dell'arte on MYmovies ONE, online </p>
             <p style={{margin:'0px', fontSize:'12px'}}>Nov 13 - 18, 2024 </p>
             <p></p>
     
-            <p style={{margin:'0px', fontSize:'14px'}}><a href="https://drive.google.com/file/d/1rRlZX-D3snkqnb9pJuvFnjcHWbDZRY-A/view" target="_blank" rel="noreferrer"><em>Times Ex-tenses</em></a></p>
-            <p style={{margin:'0px', fontSize:'12px'}}>ba̍k-nih-á film festival 2, Taipei </p>
-            <p style={{margin:'0px', fontSize:'12px'}}>May 24 - 26, 2024</p>
-            <p></p>
+        
 
     
             {/* <p style={{margin:'0px', fontSize:'14px'}}><a href="https://www.carpark.berlin/en" target="_blank" rel="noreferrer"><em>CARPARK</em></a></p>
