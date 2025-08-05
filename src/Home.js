9fbd8d7925bc93c7659d114d6ec2c082b1196a6e
geoblocked <72842644+geoblocked@users.noreply.github.com>
2025-08-05 15:24:38 +0200

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -24,6 +24,12 @@ const Home = ({hideNav}) => {
             <p style={{margin:'0px', fontSize:'12px'}}>Emalin / Antenna Space, London. Jan 18 – Feb 15, 2025 </p>
             <p></p>
 
+            <p style={{margin:'0px', fontSize:'14px'}}><a href="https://www.cellprojects.org/exhibitions/peng-zuqiang" target="_blank" rel="noreferrer"><em>Sideways Looking</em></a></p>
+            <p style={{margin:'0px', fontSize:'12px'}}>Cell Project Space, London. April 13, 2022 - June 19, 2022 </p>
+            <p style={{margin:'0px', fontSize:'12px'}}>April 13, 2022 - June 19, 2022</p>
+            <img className="landing-img" src="./images/sideways_looking.jpg" alt="exhibition poster"/>
+            <p></p>
+
 
     <p><b>Screenings :</b></p>
 
