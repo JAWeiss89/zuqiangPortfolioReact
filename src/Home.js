e47d514fe311ba0da53d94608c4891cf90217d6e
geoblocked <72842644+geoblocked@users.noreply.github.com>
2025-08-26 17:52:28 -0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -20,7 +20,15 @@ const Home = ({hideNav}) => {
             <p style={{margin:'0px', fontSize:'12px'}}>Automata, Los Angeles, US </p>
             <p style={{margin:'0px', fontSize:'12px'}}>March 26,2023</p>
             <p></p> */}
-
+<p style="margin:0; font-size:14px;">
+  <a href="https://thecommonguild.org.uk/programme/peng-zuqiang-afternoon-hearsay" 
+     target="_blank" rel="noreferrer">
+    <em>Afternoon Hearsay</em>
+  </a> — 
+  <span style="font-size:12px;">
+    The Common Guild, Glasgow, 11 October – 7 December 2025
+  </span>
+</p>
             <p style={{margin:'0px', fontSize:'14px'}}><a href="https://thecommonguild.org.uk/programme/peng-zuqiang-afternoon-hearsay" target="_blank" rel="noreferrer"><em>Afternoon Hearsay</em></a></p>
             <p style={{margin:'0px', fontSize:'12px'}}>The Common Guild, Glasgow, 11 October – 7 December 2025 </p>
             <p></p>
