911a95535203fa933f1ac426ff5b1f2c53b86fcc
geoblocked <72842644+geoblocked@users.noreply.github.com>
2026-07-20 11:41:34 +0200

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -129,7 +129,7 @@ const Home = ({ hideNav }) => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <em>International Competition</em>
+          <em>Ex-Now International Competition</em>
         </a>
       </p>
       <p style={{ margin: '0px', fontSize: '12px' }}>
